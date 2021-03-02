e06da54f15322032534ca3e8994188e97ba9c417
anjana-dev <67020473+anjana-dev@users.noreply.github.com>
2021-03-01 22:42:41 -0600

    Modified precompute, forward, backward
    Changed the implementation of Precompute basis to adjust basis and span tensor dimensions.

--- torch_nurbs_eval/csrc/surf_eval.cpp
+++ torch_nurbs_eval/csrc/surf_eval.cpp
@@ -18,56 +18,102 @@ std::vector<torch::Tensor> surf_pre_compute_basis(torch::Tensor u,
     int out_dim,
     int _dimension){
 
-  std::vector<torch::Tensor> uspan_uv;
-  std::vector<torch::Tensor> vspan_uv;
-  std::vector<torch::Tensor> Nu_uv;
-  std::vector<torch::Tensor> Nv_uv;
+
+  std::vector<int> uspan;
+  std::vector<torch::Tensor> Nu;
+
+  std::vector<int> vspan;
+  std::vector<torch::Tensor> Nv;
+
+  // std::vector<torch::Tensor> uspan_uv;
+  // std::vector<torch::Tensor> vspan_uv;
+  // std::vector<torch::Tensor> Nu_uv;
+  // std::vector<torch::Tensor> Nv_uv;
 
   float* U_ptr = (float*)U.data_ptr();
   float* V_ptr = (float*)V.data_ptr();
   auto options = torch::TensorOptions().dtype(torch::kInt);
 
 
+
   for (int i = 0; i<u.size(0); i++)
   {
-    std::vector<int> uspan_v;
-    std::vector<int> vspan_v;
-    std::vector<torch::Tensor> Nu_v;
-    std::vector<torch::Tensor> Nv_v;
-    
-    for (auto j=0; j<v.size(0); j++)
-    {
-      auto uspan = find_span(n, p, u[i].item<float>(), U_ptr);
-      auto vspan = find_span(m, q, v[j].item<float>(), V_ptr);
-      auto Nu_tensor = torch::zeros({p+1});
-      auto Nv_tensor = torch::zeros({q+1});
-      basis_funs(uspan, u[i].item<float>(), p, U_ptr, (float*)Nu_tensor.data_ptr());
-      basis_funs(vspan, v[j].item<float>(), q, V_ptr, (float*)Nv_tensor.data_ptr());
-      uspan_v.push_back(uspan);
-      vspan_v.push_back(vspan);
-      Nu_v.push_back(Nu_tensor);
-      Nv_v.push_back(Nv_tensor);
-    }
-    int *uspan_v_ptr = &uspan_v[0];
-    int *vspan_v_ptr = &vspan_v[0];
-    auto uspan_v_tensor = torch::from_blob(uspan_v_ptr, torch::IntList(v.size(0)), options).clone();
-    auto vspan_v_tensor = torch::from_blob(vspan_v_ptr, torch::IntList(v.size(0)), options).clone();
-    auto Nu_v_tensor = torch::stack(Nu_v);
-    auto Nv_v_tensor = torch::stack(Nv_v);
-    uspan_uv.push_back(uspan_v_tensor);
-    vspan_uv.push_back(vspan_v_tensor);
-    Nu_uv.push_back(Nu_v_tensor);
-    Nv_uv.push_back(Nv_v_tensor);
+    auto uspan_u = find_span(m, p, u[i].item<float>(), U_ptr);
+    auto Nu_tensor = torch::zeros({p+1});
+    basis_funs(uspan_u, u[i].item<float>(), p, U_ptr, (float*)Nu_tensor.data_ptr());
+    uspan.push_back(uspan_u);
+    Nu.push_back(Nu_tensor);
   }
 
-  auto uspan_uv_tensor = torch::stack(uspan_uv);
-  auto vspan_uv_tensor = torch::stack(vspan_uv);
-  auto Nu_uv_tensor = torch::stack(Nu_uv);
-  auto Nv_uv_tensor = torch::stack(Nv_uv);
-  return {uspan_uv_tensor,
-          vspan_uv_tensor,
-          Nu_uv_tensor,
-          Nv_uv_tensor};
+  
+
+for (int j = 0; j<v.size(0); j++)
+  {
+    auto vspan_v = find_span(n, q, v[j].item<float>(), V_ptr);
+    auto Nv_tensor = torch::zeros({q+1});
+    basis_funs(vspan_v, v[j].item<float>(), q, V_ptr, (float*)Nv_tensor.data_ptr());
+    vspan.push_back(vspan_v);
+    Nv.push_back(Nv_tensor);
+  }
+
+
+
+  int *uspan_ptr = &uspan[0];
+  auto uspan_out = torch::from_blob(uspan_ptr, torch::IntList(u.size(0)), options).clone();
+  auto Nu_out = torch::stack(Nu);
+
+  int *vspan_ptr = &vspan[0];
+  auto vspan_out = torch::from_blob(vspan_ptr, torch::IntList(v.size(0)), options).clone();
+  auto Nv_out = torch::stack(Nv);
+
+  // for (int i = 0; i<u.size(0); i++)
+  // {
+  //   std::vector<int> uspan_v;
+  //   std::vector<int> vspan_v;
+  //   std::vector<torch::Tensor> Nu_v;
+  //   std::vector<torch::Tensor> Nv_v;
+
+
+  //   auto Nu_tensor = torch::zeros({p+1});
+  //   auto uspan = find_span(n, p, u[i].item<float>(), U_ptr);
+  //   basis_funs(uspan, u[i].item<float>(), p, U_ptr, (float*)Nu_tensor.data_ptr());
+    
+  //   for (auto j=0; j<v.size(0); j++)
+  //   {
+      
+  //     auto vspan = find_span(m, q, v[j].item<float>(), V_ptr);
+      
+  //     auto Nv_tensor = torch::zeros({q+1});
+      
+  //     basis_funs(vspan, v[j].item<float>(), q, V_ptr, (float*)Nv_tensor.data_ptr());
+  //     uspan_v.push_back(uspan);
+  //     vspan_v.push_back(vspan);
+  //     Nu_v.push_back(Nu_tensor);
+  //     Nv_v.push_back(Nv_tensor);
+  //   }
+  //   int *uspan_v_ptr = &uspan_v[0];
+  //   int *vspan_v_ptr = &vspan_v[0];
+  //   auto uspan_v_tensor = torch::from_blob(uspan_v_ptr, torch::IntList(v.size(0)), options).clone();
+  //   auto vspan_v_tensor = torch::from_blob(vspan_v_ptr, torch::IntList(v.size(0)), options).clone();
+  //   auto Nu_v_tensor = torch::stack(Nu_v);
+  //   auto Nv_v_tensor = torch::stack(Nv_v);
+  //   uspan_uv.push_back(uspan_v_tensor);
+  //   vspan_uv.push_back(vspan_v_tensor);
+  //   Nu_uv.push_back(Nu_v_tensor);
+  //   Nv_uv.push_back(Nv_v_tensor);
+  // }
+
+  // auto uspan_uv_tensor = torch::stack(uspan_uv);
+  // auto vspan_uv_tensor = torch::stack(vspan_uv);
+  // auto Nu_uv_tensor = torch::stack(Nu_uv);
+  // auto Nv_uv_tensor = torch::stack(Nv_uv);
+
+
+
+  return {uspan_out,
+          vspan_out,
+          Nu_out,
+          Nv_out};
 }
 
 
@@ -96,12 +142,12 @@ torch::Tensor surf_forward(
         {
           for (int r = 0; r<=p; r++)
           {
-            temp[l] = temp[l] + Nu_uv[i][j][r].item<float>()*ctrl_pts[k][uspan_uv[i][j].item<int>() - p + r][vspan_uv[i][j].item<int>() - q + l];
+            temp[l] = temp[l] + Nu_uv[i][r].item<float>()*ctrl_pts[k][uspan_uv[i].item<int>() - p + r][vspan_uv[j].item<int>() - q + l];
           }
         }
         auto Sw = torch::zeros({_dimension+1});
         for (int l=0; l<=q; l++)
-          Sw = Sw + Nv_uv[i][j][l]*temp[l];
+          Sw = Sw + Nv_uv[j][l]*temp[l];
         surface[k][i][j] = Sw;
       }
     }
@@ -135,10 +181,10 @@ std::vector<torch::Tensor> surf_backward(
         auto grad_temp = torch::zeros({q+1,_dimension+1});
         for (int l = 0; l<=q; l++)
         {
-          grad_temp[l] = Nv_uv[i][j][l]*grad_output[k][i][j];
+          grad_temp[l] = Nv_uv[j][l]*grad_output[k][i][j];
           for (int r = 0; r<=p; r++)
           {
-            grad_ctrl_pts[k][uspan_uv[i][j].item<int>() - p + r][vspan_uv[i][j].item<int>() - q + l] = grad_ctrl_pts[k][uspan_uv[i][j].item<int>() - p + r][vspan_uv[i][j].item<int>() - q + l] + Nu_uv[i][j][r].item<float>()*grad_temp[l];
+            grad_ctrl_pts[k][uspan_uv[i].item<int>() - p + r][vspan_uv[j].item<int>() - q + l] = grad_ctrl_pts[k][uspan_uv[i].item<int>() - p + r][vspan_uv[j].item<int>() - q + l] + Nu_uv[i][r].item<float>()*grad_temp[l];
           }
         }
       }
