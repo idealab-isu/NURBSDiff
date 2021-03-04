e10300c0371e4d7be93adbc39389124277f3b39c
Aditya Balu <baditya@iastate.edu>
2021-03-03 21:18:48 -0600

    some random changes to surface fitting

--- examples/surface_fitting.py
+++ examples/surface_fitting.py
@@ -15,9 +15,9 @@ def main():
 
     num_ctrl_pts1 = 8
     num_ctrl_pts2 = 8
-    inp_ctrl_pts = torch.nn.Parameter(torch.rand(1,num_ctrl_pts1, num_ctrl_pts2, 3))
+    inp_ctrl_pts = torch.nn.Parameter(torch.rand(1,num_ctrl_pts1, num_ctrl_pts2, 4))
 
-    x = y = np.linspace(-3,3,num=64)
+    x = y = np.linspace(-5,5,num=64)
     X, Y = np.meshgrid(x, y)
 
     def fun(X,Y):
@@ -26,23 +26,25 @@ def main():
 
     zs = np.array(fun(np.ravel(X), np.ravel(Y)))
     Z = zs.reshape(X.shape)
-    target = torch.from_numpy(np.array([X,Y,Z]).T).unsqueeze(0)
-    print(target.shape)
+    target = torch.FloatTensor(np.array([X,Y,Z]).T).unsqueeze(0)
 
     layer = SurfEval(num_ctrl_pts1, num_ctrl_pts2, 3, 3, 3, 64)
     opt = torch.optim.Adam(iter([inp_ctrl_pts]), lr=0.01)
-    pbar = tqdm(range(10000))
+    pbar = tqdm(range(2000))
     for i in pbar:
         opt.zero_grad()
-        weights = torch.ones(1,num_ctrl_pts1, num_ctrl_pts2, 1)
-        out = layer(torch.cat((inp_ctrl_pts,weights),axis=-1))
-        # target = target.view(1,64*64,3)
-        # out = out.view(1,64*64,3)
-        loss = ((target-out)**2).mean()
+        # weights = torch.ones(1,num_ctrl_pts1, num_ctrl_pts2, 1)
+        out = layer(inp_ctrl_pts)
+        target = target.reshape(1,64*64,3)
+        out = out.reshape(1,64*64,3)
+        # loss = ((target-out)**2).mean()
+        loss, _ = chamfer_distance(target,out)
         loss.backward()
         opt.step()
+        target = target.reshape(1,64,64,3)
+        out = out.reshape(1,64,64,3)
 
-        if i%200 == 0:
+        if i%500 == 0:
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
             target_mpl = target.cpu().numpy().squeeze()
