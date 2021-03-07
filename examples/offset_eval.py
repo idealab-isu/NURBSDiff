Bug fix for span computation, Different surface patches for tests, Edge point mapping (WIP)

examples/offset_eval.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 def Surf_pt(u, v, P, knot_u, knot_v, degree_u, degree_v):
     count = knot_v.shape[0] - degree_v - 1
     span_u = span_linear(knot_u.shape[0] - degree_u - 1, knot_u, u)
@@ -164,29 +163,80 @@ def Basis_Deri(u, order, span, degree, knotV):
     return ders
 
 
-def compute_surf_offset(off_layers, CNTRL_PTS, knot_u, knot_v, degree_u, degree_v):
-    delta_u = 64
-    delta_v = 64
-    grid_1, grid_2 = np.meshgrid(np.linspace(0.0, 1.0, delta_u), np.linspace(0.0, 1.0, delta_v))
-    OFF_PTS = np.empty([off_layers, grid_1.shape[0], grid_1.shape[1], 3], dtype=np.float32)
-    thickness = 5.0
+def compute_normal_surface(CNTRL_PTS, knot_u, knot_v, grid_1, grid_2):
+    count = grid_1.shape[1]
+    PT = np.empty([grid_1.size, 3])
+    deri = np.empty([grid_1.size, 2, 2, 3])
+    normals = np.empty([grid_1.size, 3])
+
     for i in range(0, grid_1.shape[0]):
         for j in range(0, grid_1.shape[1]):
 
-            # PTS, normals = compute_normal_surface(CNTRL_PTS, knot_u, knot_v, grid_1, grid_2)
-            PT = Surf_pt(grid_1[i][j], grid_2[i][j], CNTRL_PTS, knot_u, knot_v, degree_u, degree_v)
-            deri = Deri_Surf(grid_1[i][j], grid_2[i][j], 1, CNTRL_PTS, knot_u, knot_v, degree_u, degree_v)
-            temp = np.cross(deri[0][1], deri[1][0])
-            normals = temp / np.linalg.norm(temp)
+            # PT[i * count + j] = Surf_pt(grid_1[i][j], grid_2[i][j], CNTRL_PTS, knot_u, knot_v, degree_u=3, degree_v=3)
+            PT[i * count + j] = Surf_pt(grid_1[i][j], grid_2[i][j], CNTRL_PTS, knot_u, knot_v, degree_u=3, degree_v=3)
+            deri[i * count + j] = Deri_Surf(grid_1[i][j], grid_2[i][j], 1, CNTRL_PTS, knot_u, knot_v, degree_u=3, degree_v=3)
+            temp = np.cross(deri[i * count + j][0][1], deri[i * count + j][1][0])
+            normals[i * count + j] = temp / np.linalg.norm(temp)
 
-            # if PT[1] < 12:
-            #     thickness = 0.5
-            # elif 40 > PT[1] > 12:
-            #     thickness = 0.25
-            # else:
-            #     thickness = 0.1
+            pass
 
-            for layer in range(0, off_layers):
-                OFF_PTS[layer][i][j] = PT + (thickness * layer * normals)
+    return PT, normals
+    pass
+
+
+def Map_Points(Surf_Pts, Normals):
+
+    EdgePointMap = np.full([Surf_Pts.shape[0], Surf_Pts.shape[1], 3], -1, dtype=np.int8)
+    count = 0
+    for i in range(EdgePointMap.shape[0]):
+        for k in range(Surf_Pts.shape[1]):
+            count = 0
+            for j in range(EdgePointMap.shape[0]):
+                if i != j:
+                    EdgePointMap[i][k][0] = count
+                    test = np.argwhere(Surf_Pts[i, k] == Surf_Pts[j])
+                    # if Surf_Pts[i, k] in Surf_Pts[j]:
+                    if test.size != 0:
+                        count += 1
+                        EdgePointMap[i][k][0] = count
+                        EdgePointMap[i][k][(2 * EdgePointMap[i][k][0]) - 1] = j
+                        EdgePointMap[i][k][(2 * EdgePointMap[i][k][0])] = test[0, 0]
+
+    for i in range(EdgePointMap.shape[0]):
+        for j in range(EdgePointMap.shape[1]):
+            if EdgePointMap[i][j][0] != 0:
+                temp = Normals[i][j]
+                for k in range(EdgePointMap[i][j][0]):
+                    temp += Normals[EdgePointMap[i][j][2 * k + 1], EdgePointMap[i][j][2 * k + 2]]
+
+                temp_norm = np.linalg.norm(temp)
+                if temp_norm != 0.0:
+                    temp /= temp_norm
+
+                Normals[i][j] = temp
+                for k in range(EdgePointMap[i][j][0]):
+                    Normals[EdgePointMap[i][j][2 * k + 1], EdgePointMap[i][j][2 * k + 2]] = temp
+                    pass
+
+    print('Max count  ==  ', count)
+
+    return Normals
+    pass
+
+
+def compute_surf_offset(CNTRL_PTS, knot_u, knot_v, degree_u, degree_v, eval_pts_size, thickness):
+    delta_u = eval_pts_size
+    delta_v = eval_pts_size
+    grid_1, grid_2 = np.meshgrid(np.linspace(0.0, 1.0, delta_u), np.linspace(0.0, 1.0, delta_v))
+    OFF_PTS = np.empty([CNTRL_PTS.shape[0], grid_1.shape[0] * grid_1.shape[1], 3], dtype=np.float32)
+    SURF_PTS = np.empty([CNTRL_PTS.shape[0], grid_1.shape[0] * grid_1.shape[1], 3], dtype=np.float32)
+    NORMALS = np.empty([CNTRL_PTS.shape[0], grid_1.shape[0] * grid_1.shape[1], 3], dtype=np.float32)
+
+    for i in range(0, CNTRL_PTS.shape[0]):
+        SURF_PTS[i], NORMALS[i] = compute_normal_surface(CNTRL_PTS[i], knot_u, knot_v, grid_1, grid_2)
+        OFF_PTS[i] = SURF_PTS[i] + (thickness * NORMALS[i])
+
+    # NORMALS = Map_Points(SURF_PTS, NORMALS)
+    # for i in range(CNTRL_PTS.shape[0]):
 
     return OFF_PTS

examples/surface_offsetting.py
@@ -12,29 +12,50 @@ import offset_eval as off
 
 def main():
     timing = []
+    eval_pts_size = 20
 
+    # Turbine Blade Surfaces
+    # num_ctrl_pts1 = 50
+    # num_ctrl_pts2 = 24
+    # ctrl_pts_2 = np.load('TurbineBladeCtrlPts.npy').astype('float32')
+    # knot_u = np.load('TurbineKnotU.npy')
+    # knot_v = np.load('TurbineKnotV.npy')
 
-    num_ctrl_pts1 = 4
-    num_ctrl_pts2 = 4
-    ctrl_pts_2 = np.load('CNTRL_PTS_2_Chamber.npy').astype('float32')
-    ctrl_pts_2[:,:,:,-1] = 1.0
-    temp = np.reshape(ctrl_pts_2, [200, 972, num_ctrl_pts1, num_ctrl_pts2, 4])
-    isolate_pts = torch.from_numpy(temp[0, 1:2, :, :, :3])
+    # Cardiac Model Surfaces
+    # num_ctrl_pts1 = 4
+    # num_ctrl_pts2 = 4
+    # ctrl_pts_2 = np.load('CNTRL_PTS_2_Chamber.npy').astype('float32')
+    # ctrl_pts_2[:, :, :, -1] = 1.0
+    # Element_Array = np.array([24, 30])
+    # ctrl_pts_2 = ctrl_pts_2[0, :, :, :]
+    # knot_u = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
+    # knot_v = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
+
+    #
+    num_ctrl_pts1 = 6
+    num_ctrl_pts2 = 6
+    ctrl_pts_2 = np.load('CtrlPtsRoof.npy').astype('float32')
+    Element_Array = np.array([0])
+    ctrl_pts_2 = ctrl_pts_2[:, :, :]
+    knot_u = np.array([0.0, 0.0, 0.0, 0.0, 0.33, 0.67, 1.0, 1.0, 1.0, 1.0])
+    knot_v = np.array([0.0, 0.0, 0.0, 0.0, 0.33, 0.67, 1.0, 1.0, 1.0, 1.0])
+
+    off_pts = off.compute_surf_offset(ctrl_pts_2[Element_Array], knot_u, knot_v, 3, 3, eval_pts_size, 1.0)
+    # target = torch.from_numpy(np.reshape(off_pts, [Element_Array.size, eval_pts_size, eval_pts_size, 3])).cuda()
+    target = torch.from_numpy(np.reshape(off_pts, [Element_Array.size, eval_pts_size, eval_pts_size, 3]))
+
+    temp = np.reshape(ctrl_pts_2[Element_Array], [ctrl_pts_2[Element_Array].shape[0], num_ctrl_pts1, num_ctrl_pts2, 4])
+    # isolate_pts = torch.from_numpy(temp).cuda()
+    isolate_pts = torch.from_numpy(temp)
     inp_ctrl_pts = torch.nn.Parameter(isolate_pts)
-    knot_u = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
-    knot_v = np.array([0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
-    off_pts = off.compute_surf_offset(1, ctrl_pts_2[0, 0], knot_u, knot_v, 3, 3)
-    target = torch.from_numpy(off_pts)
 
-    layer = SurfEval(num_ctrl_pts1, num_ctrl_pts2, 3, 3, 3, 64)
+    layer = SurfEval(num_ctrl_pts1, num_ctrl_pts2, knot_u, knot_v, 3, 3, 3, eval_pts_size)
     opt = torch.optim.Adam(iter([inp_ctrl_pts]), lr=0.01)
     pbar = tqdm(range(10000))
     for i in pbar:
         opt.zero_grad()
-        weights = torch.ones(1,num_ctrl_pts1, num_ctrl_pts2, 1)
-        out = layer(torch.cat((inp_ctrl_pts,weights),axis=-1))
-        # target = target.view(1,64*64,3)
-        # out = out.view(1,64*64,3)
+        # weights = torch.ones(1,num_ctrl_pts1, num_ctrl_pts2, 1)
+        out = layer(inp_ctrl_pts)
         loss = ((target-out)**2).mean()
         loss.backward()
         opt.step()
@@ -42,19 +63,32 @@ def main():
         if i%200 == 0:
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
-            target_mpl = target.numpy().squeeze()
-            predicted = out.detach().numpy().squeeze()
-            surf1 = ax.plot_surface(target_mpl[:, :,0],target_mpl[:, :,1],target_mpl[:, :,2], color='blue', label='target')
-            surf2 = ax.plot_surface(predicted[:, :,0], predicted[:, :,1], predicted[:, :,2], color='green', label='predicted')
-            surf1._facecolors2d=surf1._facecolor3d
-            surf1._edgecolors2d=surf1._edgecolor3d
-            surf2._facecolors2d=surf2._facecolor3d
-            surf2._edgecolors2d=surf2._edgecolor3d
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-            ax.legend()
-            ax.view_init(elev=20., azim=-35)
+
+            for k in range(target.shape[0]):
+                # target_mpl = target[k:k+1].cpu().numpy().squeeze()
+                # predicted = out[k:k+1].detach().cpu().numpy().squeeze()
+                target_mpl = target[k:k+1].numpy().squeeze()
+                predicted = out[k:k+1].detach().numpy().squeeze()
+
+                surf1 = ax.plot_surface(target_mpl[:, :,0],target_mpl[:, :,1],target_mpl[:, :,2], color='blue', label='target')
+                surf2 = ax.plot_surface(predicted[:, :,0], predicted[:, :,1], predicted[:, :,2], color='green', label='predicted')
+
+                # Get point cloud of evaluated points
+                target_2 = np.reshape(target_mpl, [eval_pts_size * eval_pts_size, 3])
+                out_2 = np.reshape(predicted, [eval_pts_size * eval_pts_size, 3])
+
+                # np.savetxt('TargetSurface_Iter_%d.csv' % i, target_2)
+                # np.savetxt('PredictSurface_Iter_%d_%d.csv' % (k, i), out_2)
+
+            # surf1._facecolors2d=surf1._facecolor3d
+            # surf1._edgecolors2d=surf1._edgecolor3d
+            # surf2._facecolors2d=surf2._facecolor3d
+            # surf2._edgecolors2d=surf2._edgecolor3d
+            # ax.set_xlabel('X Label')
+            # ax.set_ylabel('Y Label')
+            # ax.set_zlabel('Z Label')
+            # ax.legend()
+            # ax.view_init(elev=20., azim=-35)
             plt.show()
         pbar.set_description("Loss %s: %s" % (i+1, loss.item()))
 

torch_nurbs_eval/surf_eval.py
@@ -7,7 +7,8 @@ import numpy as np
 # from torch.utils.cpp_extension import load
 # surf_eval_cuda = load(
 #     'surf_eval_cuda', ['csrc\\surf_eval.cpp', 'csrc\\surf_eval_cuda_kernel.cu'], verbose=True)
-from surf_eval_cuda import pre_compute_basis, forward, backward
+# from torch_nurbs_eval.surf_eval_cuda import pre_compute_basis, forward, backward
+from torch_nurbs_eval.surf_eval_cpp import pre_compute_basis, forward, backward
 # from surf_eval_cpp import  forward as forward_cpp, backward as backward_cpp
 import surface_data_generator as dg
 import time
@@ -42,16 +43,21 @@ class SurfEval(torch.nn.Module):
     torch.autograd.Function and implementing the forward and backward passes
     which operate on Tensors.
     """
-    def __init__(self, m, n, dimension=3, p=3, q=3, out_dim=64):
+    def __init__(self, m, n, knot_u, knot_v, dimension=3, p=3, q=3, out_dim=64):
         super(SurfEval, self).__init__()
         self.m = m
         self.n = n
         self._dimension = dimension
         self.p, self.q = p, q
-        self.U = torch.Tensor(np.array(gen_knot_vector(self.p, self.m))).to('cuda')
-        self.V = torch.Tensor(np.array(gen_knot_vector(self.q, self.n))).to('cuda')
-        self.u = torch.linspace(0.0, 0.99, steps=out_dim,dtype=torch.float32,device='cuda')
-        self.v = torch.linspace(0.0, 0.99, steps=out_dim,dtype=torch.float32,device='cuda')
+        # self.U = torch.Tensor(knot_u).to('cuda')
+        # self.V = torch.Tensor(knot_v).to('cuda')
+        # self.u = torch.linspace(0.0, 1.0, steps=out_dim,dtype=torch.float32,device='cuda')
+        # self.v = torch.linspace(0.0, 1.0, steps=out_dim,dtype=torch.float32,device='cuda')
+
+        self.U = torch.Tensor(knot_u)
+        self.V = torch.Tensor(knot_v)
+        self.u = torch.linspace(0.0, 1.0, steps=out_dim,dtype=torch.float32)
+        self.v = torch.linspace(0.0, 1.0, steps=out_dim,dtype=torch.float32)
         self.uspan_uv, self.vspan_uv, self.Nu_uv, self.Nv_uv = pre_compute_basis(self.u, self.v, self.U, self. V, m, n, p , q, out_dim, self._dimension)
         self.Nu_uv = self.Nu_uv.view(out_dim, p+1)
         self.Nv_uv = self.Nu_uv.view(out_dim, q+1)
@@ -138,7 +144,8 @@ class SurfEvalFunc(torch.autograd.Function):
         q = ctx.q
         _dimension = ctx._dimension
         surfaces=ctx.surfaces
-        grad_sw = torch.zeros((grad_output.size(0),grad_output.size(1),grad_output.size(2),_dimension+1),dtype=torch.float32,device='cuda')
+        grad_sw = torch.zeros((grad_output.size(0),grad_output.size(1),grad_output.size(2),_dimension+1),dtype=torch.float32)
+        # grad_sw = torch.zeros((grad_output.size(0),grad_output.size(1),grad_output.size(2),_dimension+1),dtype=torch.float32,device='cuda')
         grad_sw[:,:,:,:_dimension] = grad_output
         for d in range(_dimension):
             grad_sw[:,:,:,_dimension] += grad_output[:,:,:,d]/surfaces[:,:,:,_dimension]
@@ -146,7 +153,7 @@ class SurfEvalFunc(torch.autograd.Function):
 
 
         # print("Before backward")
-        grad_ctrl_pts  = backward(grad_sw, ctrl_pts, uspan_uv, vspan_uv, Nu_uv, Nv_uv, u_uv, v_uv, m, n, p, q, _dimension)
+        grad_ctrl_pts = backward(grad_sw, ctrl_pts, uspan_uv, vspan_uv, Nu_uv, Nv_uv, u_uv, v_uv, m, n, p, q, _dimension)
         
         
         #Checks for backward