6f59ac5489cc732c3c5c7f1c81723c30fa9171e8
Harshil Shah <harshil@iastate.edu>
2021-04-23 14:57:42 -0500

    One sided Chamfer distance and surface regularization.

--- examples/ValveReconstruction/ValveReconstruct.py
+++ examples/ValveReconstruction/ValveReconstruct.py
@@ -1,35 +1,68 @@
 import torch
 import numpy as np
+
 torch.manual_seed(120)
 from tqdm import tqdm
 from pytorch3d.loss import chamfer_distance
 from torch_nurbs_eval.surf_eval import SurfEval
 import matplotlib.pyplot as plt
+from torch.autograd import Variable
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import offset_eval as off
 import sys
+import copy
 
 from geomdl import NURBS, multi, exchange
 from geomdl.visualization import VisMPL
 import CPU_Eval as cpu
 
 
-def main():
-    evalPtSize = 100
+def chamfer_distance_one_side(pred, gt, side=1):
+    """
+    Computes average chamfer distance prediction and groundtruth
+    but is one sided
+    :param pred: Prediction: B x N x 3
+    :param gt: ground truth: B x M x 3
+    :return:
+    """
+    if isinstance(pred, np.ndarray):
+        pred = Variable(torch.from_numpy(pred.astype(np.float32))).cuda()
 
-    f = open('smesh_leaf_anterior.1.dat')
+    if isinstance(gt, np.ndarray):
+        gt = Variable(torch.from_numpy(gt.astype(np.float32))).cuda()
+
+    pred = torch.unsqueeze(pred, 1)
+    gt = torch.unsqueeze(gt, 2)
+
+    diff = pred - gt
+    diff = torch.sum(diff ** 2, 3)
+    if side == 0:
+        cd = torch.mean(torch.min(diff, 1)[0], 1)
+    elif side == 1:
+        cd = torch.mean(torch.min(diff, 2)[0], 1)
+    cd = torch.mean(cd)
+    return cd
+
+
+def main():
+    evalPtSize = 25
+
+    f = open('smesh.3.dat')
     lines = f.readlines()
 
     order = int(lines[0])
     degree = np.array(list(map(int, lines[1].split(' '))))
     CtrlPtsCountUV = list(map(int, lines[2].split(' ')))
 
+    CtrlPtsCountUV[0] = 5
+
     CtrlPtsTotal = CtrlPtsCountUV[0] * CtrlPtsCountUV[1]
 
     knotV = np.array(list(map(float, lines[3].split())))
     knotV -= knotV[0]
     knotV /= knotV[-1]
+    knotV = np.array([0, 0, 0, 0, 0.5, 1, 1, 1, 1])
 
     knotU = np.array(list(map(float, lines[4].split())))
     knotU -= knotU[0]
@@ -48,23 +81,44 @@ def main():
     layer = SurfEval(CtrlPtsCountUV[1], CtrlPtsCountUV[0], knot_u=knotU, knot_v=knotV, dimension=3,
                      p=degree[0], q=degree[1], out_dim_u=evalPtSize, out_dim_v=evalPtSize)
 
-    inpCtrlPts = torch.nn.Parameter(torch.from_numpy(CtrlPtsNoW))
+    inpCtrlPts = torch.nn.Parameter(torch.from_numpy(copy.deepcopy(CtrlPtsNoW)))
+
+    weight = torch.ones(1, CtrlPtsCountUV[1], CtrlPtsCountUV[0], 1)
+    out_2 = layer(torch.cat((inpCtrlPts.unsqueeze(0), weight), axis=-1))
+
+    BaseAreaSurf = out_2.detach().cpu().numpy().squeeze()
+
+    length1 = ((BaseAreaSurf[0:-1, :, :] - BaseAreaSurf[1:, :, :]) ** 2).sum(-1).squeeze()
+    length2 = ((BaseAreaSurf[:, 0:-1, :] - BaseAreaSurf[:, 1:, :]) ** 2).sum(-1).squeeze()
+    surf_areas_base = np.matmul(length1, length2)
+
+    baseval = np.sum(surf_areas_base)
+
     opt = torch.optim.Adam(iter([inpCtrlPts]), lr=0.01)
     pbar = tqdm(range(20000))
     for i in pbar:
         opt.zero_grad()
 
+        weight = torch.ones(1, CtrlPtsCountUV[1], CtrlPtsCountUV[0], 1)
+        out = layer(torch.cat((inpCtrlPts.unsqueeze(0), weight), axis=-1))
+
+        length1 = ((out[:, 0:-1, :, :] - out[:, 1:, :, :]) ** 2).sum(-1).squeeze()
+        length2 = ((out[:, :, 0:-1, :] - out[:, :, 1:, :]) ** 2).sum(-1).squeeze()
+        surf_areas = torch.matmul(length1, length2)
+
+        lossCD1Side = chamfer_distance_one_side(out.view(1, evalPtSize * evalPtSize, 3), target.view(1, 360, 3))
+        # loss, _ = chamfer_distance(target.view(1, 360, 3), out.view(1, evalPtSize * evalPtSize, 3))
+        # if int(i/50)%2 == 1:
+        lossCD1Side += (0.1) * torch.abs(surf_areas.sum() - baseval)
+        lossCD1Side += (0.01) * torch.abs(surf_areas.sum())
+        lossCD1Side.backward()
+        opt.step()
+
         # Fixing U = 0 Ctrl Pts
         for j in range(0, CtrlPtsCountUV[1]):
             inpCtrlPts.data[j][0] = torch.from_numpy(CtrlPtsNoW[j][0])
 
-        weight = torch.ones(1, CtrlPtsCountUV[1], CtrlPtsCountUV[0], 1)
-        out = layer(torch.cat((inpCtrlPts.unsqueeze(0), weight), axis=-1))
-        loss, _ = chamfer_distance(target.view(1, 360, 3), out.view(1, evalPtSize * evalPtSize, 3))
-        loss.backward()
-        opt.step()
-
-        if i % 100 == 0:
+        if i % 500 == 0:
             fig = plt.figure(figsize=(4, 4))
             ax = fig.add_subplot(111, projection='3d', adjustable='box', proj_type='ortho')
 
@@ -72,10 +126,13 @@ def main():
             predicted = out.detach().cpu().numpy().squeeze()
             predCtrlPts = inpCtrlPts.detach().cpu().numpy().squeeze()
 
-            surf1 = ax.scatter(target_cpu[:, 0], target_cpu[:, 1], target_cpu[:, 2], s=1.0, color='red')
-            surf2 = ax.plot_surface(predicted[:, :, 0], predicted[:, :, 1], predicted[:, :, 2], color='green')
+            surf1 = ax.scatter(target_cpu[:, 0], target_cpu[:, 1], target_cpu[:, 2], s=3.0, color='red')
+            surf2 = ax.plot_surface(predicted[:, :, 0], predicted[:, :, 1], predicted[:, :, 2], color='green',
+                                    alpha=0.5)
             # surf2 = ax.plot_wireframe(predCtrlPts[:, :, 0], predCtrlPts[:, :, 1], predCtrlPts[:, :, 2], linewidth=1,
             #                   linestyle='dashed', color='orange')
+            ax.plot(CtrlPtsNoW[:, 0, 0], CtrlPtsNoW[:, 0, 1], CtrlPtsNoW[:, 0, 2], linewidth=3,
+                    linestyle='solid', color='green')
 
             ax.azim = -90
             ax.dist = 6.5
@@ -95,7 +152,7 @@ def main():
             fig.tight_layout()
             plt.show()
 
-        pbar.set_description("Chamfer Loss is %s: %s" % (i + 1, loss))
+        pbar.set_description("Chamfer Loss is %s: %s" % (i + 1, lossCD1Side))
     surf = NURBS.Surface()
     surf.delta = 0.1
 
@@ -119,4 +176,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
