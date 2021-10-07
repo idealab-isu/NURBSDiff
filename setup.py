43bd3eaad5ff927ed65494db531be827d8c21517
adityabalu <baditya@iastate.edu>
2021-10-07 11:26:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ import os
 
 setup(
     name='torch_nurbs_eval',
-    ext_modules=[
-        CppExtension(name='torch_nurbs_eval.curve_eval_cpp',
-            sources=['torch_nurbs_eval/csrc/curve_eval.cpp','torch_nurbs_eval/csrc/utils.cpp'],
-            extra_include_paths=['torch_nurbs_eval/csrc/utils.h','torch_nurbs_eval/csrc/curve_eval.h']),
-        CppExtension(name='torch_nurbs_eval.surf_eval_cpp',
-            sources=['torch_nurbs_eval/csrc/surf_eval.cpp','torch_nurbs_eval/csrc/utils.cpp'],
-            extra_include_paths=['torch_nurbs_eval/csrc/utils.h','torch_nurbs_eval/csrc/surf_eval.h']),
-        CUDAExtension(name='torch_nurbs_eval.curve_eval_cuda',
-            sources=['torch_nurbs_eval/csrc/curve_eval_cuda.cpp',
-            'torch_nurbs_eval/csrc/curve_eval_cuda_kernel.cu']),
-        CUDAExtension(name='torch_nurbs_eval.surf_eval_cuda',
-            sources=['torch_nurbs_eval/csrc/surf_eval_cuda.cpp',
-            'torch_nurbs_eval/csrc/surf_eval_cuda_kernel.cu']),
-    ],
-    cmdclass={
-        'build_ext': BuildExtension
-    },
+#     ext_modules=[
+#         CppExtension(name='torch_nurbs_eval.curve_eval_cpp',
+#             sources=['torch_nurbs_eval/csrc/curve_eval.cpp','torch_nurbs_eval/csrc/utils.cpp'],
+#             extra_include_paths=['torch_nurbs_eval/csrc/utils.h','torch_nurbs_eval/csrc/curve_eval.h']),
+#         CppExtension(name='torch_nurbs_eval.surf_eval_cpp',
+#             sources=['torch_nurbs_eval/csrc/surf_eval.cpp','torch_nurbs_eval/csrc/utils.cpp'],
+#             extra_include_paths=['torch_nurbs_eval/csrc/utils.h','torch_nurbs_eval/csrc/surf_eval.h']),
+#         CUDAExtension(name='torch_nurbs_eval.curve_eval_cuda',
+#             sources=['torch_nurbs_eval/csrc/curve_eval_cuda.cpp',
+#             'torch_nurbs_eval/csrc/curve_eval_cuda_kernel.cu']),
+#         CUDAExtension(name='torch_nurbs_eval.surf_eval_cuda',
+#             sources=['torch_nurbs_eval/csrc/surf_eval_cuda.cpp',
+#             'torch_nurbs_eval/csrc/surf_eval_cuda_kernel.cu']),
+#     ],
+#     cmdclass={
+#         'build_ext': BuildExtension
+#     },
     packages=find_packages(),)
